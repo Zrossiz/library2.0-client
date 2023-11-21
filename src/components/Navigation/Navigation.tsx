f6a496706357444cf114df2f6c3e8cfe11b3ab38
Egor <96371791+Zrossiz@users.noreply.github.com>
2023-11-21 17:32:45 +0300

    feat: select active link by path

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { useRouter } from "next/router";
+import cn from "classnames";
 import styles from "./Navigation.module.scss";
 import { LinkItem } from "@/elements";
 import { navData } from "@/helpers/constants";
 
 export const Navigation = () => {
+  const router = useRouter();
+  const pathname = router.pathname;
+  console.log(pathname);
+  console.log(router.asPath);
   return (
     <nav className={styles.wrapper}>
       {navData.map((item, index) => (
-        <LinkItem icon={item.icon} href={item.href} key={index}>
+        <LinkItem
+          className={cn({
+            [styles.active]: pathname == item.href,
+          })}
+          icon={item.icon}
+          href={item.href}
+          key={index}
+        >
           {item.name}
         </LinkItem>
       ))}
